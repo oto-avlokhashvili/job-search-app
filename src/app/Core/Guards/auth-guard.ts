c8d017ab385d514e602c2f2efbff4c75296b3cc0
otonika10 <oto.avloxashvili10@gmail.com>
2026-05-10 16:17:28 +0400

    Fix-guard

--- src/app/Core/Guards/auth-guard.ts
+++ src/app/Core/Guards/auth-guard.ts
@@ -1,15 +1,17 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router, UrlTree } from '@angular/router';
+import { CanActivateFn, UrlTree, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { AuthService } from '../Services/auth-service';
 
-export const authGuard: CanActivateFn = (): boolean | UrlTree => {
+export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot): boolean | UrlTree => {
   const service = inject(AuthService);
   const router = inject(Router);
-  const publicRoutes = ['/auth/google', '/auth/google/callback'];
 
-  if (publicRoutes.some(r => router.url.startsWith(r))) {
+  // If there's a token in the URL, let it through — layout will extract it
+  const urlToken = route.queryParamMap.get('token');
+  if (urlToken) {
     return true;
   }
+
   if (service.isLoggedIn()) {
     return true;
   } else {
